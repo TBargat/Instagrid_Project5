75081173ed621dbd7dd35acf7dadd0142e86ba49
TBargat <tbargat@gmail.com>
2018-06-08 20:53:25 +0200

    Responsive in portrait

---
+++ Instagrid_Project5/View/PictureCanvasView.swift
@@ -0,0 +1,14 @@
+//
+//  PictureCanvasView.swift
+//  Instagrid_Project5
+//
+//  Created by Thibault Dev on 08/06/2018.
+//  Copyright © 2018 Thibault Dev. All rights reserved.
+//
+
+import UIKit
+
+class PictureCanvasView: UIView {
+
+
+}
